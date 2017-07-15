b067cdfa23d20d08ff77444b99478d26f1b2a31a
Michael Clausen <michaeljclausen@gmail.com>
2017-07-15 13:39:32 -0700

    Implements host subdomain in server/app.js

--- server/app.js
+++ server/app.js
@@ -3,8 +3,12 @@ const express = require('express');
 const path = require('path');
 const middleware = require('./middleware');
 const routes = require('./routes');
+const subdomain = require('subdomain');
+
 
 const app = express();
+
+app.use(subdomain({ base : process.env.DOMAIN || 'localhost', removeWWW : true }));
 
 app.use(middleware.morgan('dev'));
 app.use(middleware.cookieParser());
@@ -20,6 +24,7 @@ app.use(middleware.flash());
 
 app.use(express.static(path.join(__dirname, '../public')));
 
+app.use('/subdomain/host/', routes.auth);
 app.use('/', routes.auth);
 app.use('/api', routes.api);
 app.use('/api/profiles', routes.profiles);
