f10d6a459aa993627e6bff0147bc2e5b0c7bbaa7
Sheyda Rezaei <sheydarezaee@gmail.com>
2017-07-20 15:13:57 -0700

    fixed eslint error

--- server/middleware/socketIO.js
+++ server/middleware/socketIO.js
@@ -11,6 +11,78 @@ module.exports = server => {
         socket.emit('action', {type: 'testSocket_ServerToClient', data: 'Socket data flow from server to client confirmed'});
       }
     });
+
+    socket.on('action', (action) => {
+      // if (action.type === 'server/testSocket_ClientToServer') {
+      //   console.log('data: ', action.data);
+
+      //   socket.emit('action', {type: 'testSocket_ServerToClient', data: 'Socket data flow from server to client confirmed'});
+      // }
+
+      let user = {
+        admin: 2,
+        id: 9,
+        firstName: 'Sheyda',
+        lastName: 'Rezaei',
+        email: 'skysurfer@gmail.com',
+        phone: '415-316-6314'
+      };
+
+      if (user.admin) {
+        socket.emit('action', {type: 'UPDATE_QUEUE_INFO_HOST', 
+          payload: {
+            partyCount: 6,
+            waitDuration: '38min-host',
+            waitTime: '8:49pm-host',
+            queueList: [
+              {
+                user: {
+                  id: 9,
+                  firstName: 'Sheyda',
+                  lastName: 'Rezaei',
+                  email: 'skysurfer@gmail.com',
+                  phone: '415-316-6314'
+                },
+                partyCount: 3,
+                waitDuration: '22min',
+                waitTime: '8:25pm'
+              },
+              {
+                user: {
+                  id: 7,
+                  firstName: 'Fariba',
+                  lastName: 'Rezae',
+                  email: 'fariba@gmail.com',
+                  phone: '415-756-7465'
+                },
+                partyCount: 3,
+                waitDuration: '22min',
+                waitTime: '8:25pm'
+              }
+            ]
+          }});
+      } else {
+        socket.emit('action', {type: 'UPDATE_QUEUE_INFO_CUSTOMER', 
+          payload: {
+            partyCount: 6,
+            waitDuration: '38min',
+            waitTime: '8:49pm'
+          }});
+
+        socket.emit('action', {type: 'UPDATE_PARTY_INFO_CUSTOMER', 
+          payload: {
+            partyId: 8,
+            partiesAhead: 8,
+            partiesBehind: 10,
+            myWaitDuration: '20min',
+            myWaitTime: '8:35pm',
+            message: 'please head to the queue' 
+          }});
+
+      }
+    });
+    
+
   });
 };
 
