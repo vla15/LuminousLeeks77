274cbf27ea0b098dfe0d2dc7ec16f60d788396a0
Michael Clausen <michaeljclausen@gmail.com>
2017-07-22 12:00:44 -0700

    Adds socket_id to profile on connection

--- db/migrations/20170326215143_initial.js
+++ db/migrations/20170326215143_initial.js
@@ -9,6 +9,7 @@ exports.up = function (knex, Promise) {
       table.string('email', 100).nullable().unique();
       table.string('phone', 100).nullable();
       table.string('admin', 100).nullable();
+      table.string('socket_id', 100).nullable();
       table.timestamps(true, true);
     }),
     knex.schema.createTableIfNotExists('auths', function(table) {
