d0f674415c5488fe5f7fe93400e11fab9add5b7e
Vincent La <vincentla@Vincents-MacBook-Pro.local>
2017-07-23 18:35:17 -0700

    (feat) renders partyInfo view for a customer who has enqueued after a refresh

--- client/src/actions/partyActions.js
+++ client/src/actions/partyActions.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
 import { ENQUEUE, DEQUEUE, GET_PARTY_INFO, UPDATE_PARTY_SIZE, UPDATE_FIRST_NAME, UPDATE_PHONE_NUMBER } from './actionTypes.js';
 
-const getPartyInfo = store => {
-  // return dispatch => {
-  //   axios.get('/api/partyInfo', { params: { queueId: 1, userId: store.user.id } })
-  //   .then(result => {
-  //     dispatch({
-  //       type: GET_PARTY_INFO,
-  //       payload: result.data
-  //     });
-  //   });
-  // };
+const getPartyInfo = (queue_id, user_id) => {
+  return dispatch => {
+    axios.get(`/api/partyInfo/${queue_id}/${user_id}`)
+      .then(result => {
+        dispatch({
+          type: GET_PARTY_INFO,
+          payload: result.data[0]
+        });
+      });
+  };
 };
 
 const dequeue = (queue_id, party_id) => {
@@ -28,13 +28,13 @@ const dequeue = (queue_id, party_id) => {
 const enqueue = (user_id, queue_id, party_size, first_name, phone_number) => {
   return dispatch => {
     axios.put(`/api/partyInfo/add/${queue_id}/${user_id}/${party_size}/${first_name}/${phone_number}`)
-    .then(result => {
-      console.log('result', result);
-      dispatch({
-        type: ENQUEUE,
-        payload: result.data[0]
+      .then(result => {
+        console.log('result', result);
+        dispatch({
+          type: ENQUEUE,
+          payload: result.data[0]
+        });
       });
-    });
   };
 };
 
@@ -44,22 +44,22 @@ const updatePartySize = partySize => {
     return {
       type: UPDATE_PARTY_SIZE,
       payload: partySize
-    }
+    };
   }
-}
+};
 
 const updateFirstName = firstName => {
   return {
     type: UPDATE_FIRST_NAME,
     payload: firstName
-  }
-}
+  };
+};
 
 const updatePhoneNumber = phoneNumber => {
   return {
     type: UPDATE_PHONE_NUMBER,
     payload: phoneNumber
-  }
-}
+  };
+};
 
 export { enqueue, dequeue, getPartyInfo, updatePartySize, updateFirstName, updatePhoneNumber };
--- server/controllers/party.js
+++ server/controllers/party.js
@@ -19,14 +19,18 @@ module.exports.getOne = (req, res) => {
 //gets all parties for the host;
 //passing in queue Id and partyId
 module.exports.getPartyInfoCustomer = (req, res) => {
-  var queue = [];
-  return models.Party.where({queue_id: req.params.queueid})
-    .query((qb) => {
-      qb.orderBy('wait_time', 'ASC');
-    })
-    .fetchAll({
-      withRelated: ['queue', 'profile'],
-      columns: ['id', 'queue_id', 'wait_time', 'profile_id', 'party_size']
+  return models.Party.where({profile_id: req.params.userid})
+    .fetch({require: true})
+    .then(party => {
+      res.party_id = res.party_id || party.get('id');
+      return models.Party.where({queue_id: req.params.queueid})
+        .query((qb) => {
+          qb.orderBy('wait_time', 'ASC');
+        })
+        .fetchAll({
+          withRelated: ['queue', 'profile'],
+          columns: ['id', 'queue_id', 'wait_time', 'profile_id', 'party_size']
+        });
     })
     .then(result => {
       var length = result.length;
@@ -38,11 +42,10 @@ module.exports.getPartyInfoCustomer = (req, res) => {
       targetCustomer = targetCustomer.filter(party => {
         return party.get('id') === Number(res.party_id);
       });
-      console.log(targetCustomer);
       res.send(targetCustomer);
     })
-    .error(err => {
-      res.send(err);
+    .catch(err => {
+      res.sendStatus(404);
     });
 };
 
@@ -51,70 +54,69 @@ module.exports.enqueue = (req, res, next) => {
   if (req.isAuthenticated()) {
     models.Profile.where({id: req.params.userid})
       .fetch()
-    .then(user => {
-      if (user && user.get('admin') !== '1') {
-        throw user;
-      } else {
-        return models.Profile.forge({
-          phone: req.params.phonenumber,
-          first: req.params.firstname
-        }).save()
-        .then(user => {
-          req.params.userid = user.get('id');
+      .then(user => {
+        if (user && user.get('admin') !== '1') {
           throw user;
-        })
-      }
-    })
-    .catch(user => {
-      return models.Queue.where({id: req.params.queueid})
-        .fetch({columns: ['next_wait_time', 'is_open']})
-        .then(result => {
-        if (!result.get('is_open')) {
-          throw result;
         } else {
-          return models.Party.forge({
-            queue_id: req.params.queueid,
-            wait_time: moment(new Date()).add(result.get('next_wait_time'), 'm'),
-            profile_id: req.params.userid,
-            party_size: req.params.partysize,
-            first_name: user.get('first'),
-            phone_number: user.get('phone')
-            }).save()
-            .then(result => { res.party_id = result.get('id');})
-            .error(err => {
-              res.send(err);
-            })
+          return models.Profile.forge({
+            phone: req.params.phonenumber,
+            first: req.params.firstname
+          }).save()
+            .then(user => {
+              req.params.userid = user.get('id');
+              throw user;
+            });
         }
       })
-      .then((party) => {
-        console.log('party', party)
-        return models.Party.where({queue_id: req.params.queueid})
-          .count('id');
-      })
-      .then(count => {
+      .catch(user => {
         return models.Queue.where({id: req.params.queueid})
-          .save({queue_size: count}, {patch: true});
-      })
-      .then(success => {
-        return next();
-        let queueSize = success.attributes.queue_size;
-        console.log(success.get('queue_size'));
-        // send new queue size to all the clients in the queue
-        models.Party.where({queue_id: req.params.queueid})
-          .fetchAll()
-          .then(parties => {
-            parties.each((party, i) => {
-              //get the socketID for each user
-              console.log('party', i);
-            });
+          .fetch({columns: ['next_wait_time', 'is_open']})
+          .then(result => {
+            if (!result.get('is_open')) {
+              throw result;
+            } else {
+              return models.Party.forge({
+                queue_id: req.params.queueid,
+                wait_time: moment(new Date()).add(result.get('next_wait_time'), 'm'),
+                profile_id: req.params.userid,
+                party_size: req.params.partysize,
+                first_name: user.get('first'),
+                phone_number: user.get('phone')
+              }).save()
+                .then(result => { res.party_id = result.get('id'); })
+                .error(err => {
+                  res.send(err);
+                });
+            }
+          })
+          .then((party) => {
+            return models.Party.where({queue_id: req.params.queueid})
+              .count('id');
+          })
+          .then(count => {
+            return models.Queue.where({id: req.params.queueid})
+              .save({queue_size: count}, {patch: true});
+          })
+          .then(success => {
+            return next();
+            let queueSize = success.attributes.queue_size;
+            console.log(success.get('queue_size'));
+            // send new queue size to all the clients in the queue
+            models.Party.where({queue_id: req.params.queueid})
+              .fetchAll()
+              .then(parties => {
+                parties.each((party, i) => {
+                  //get the socketID for each user
+                  console.log('party', i);
+                });
+              });
+            //getAllPartiesInQueue(req.params.queueId);
+            res.status(200).send('successful');
+          })
+          .error(err => {
+            res.send(404);
           });
-        //getAllPartiesInQueue(req.params.queueId);
-        res.status(200).send('successful');
-      })
-      .error(err => {
-        res.send(404);
-      })
-    })
+      });
   } else {
     res.send('you aint authenticated');
   }
