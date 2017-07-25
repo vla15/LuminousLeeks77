12f373679fdd23618a08f7abe19ae221ec53cbb3
Vincent La <vincentla@Vincents-MacBook-Pro.local>
2017-07-25 11:57:48 -0700

    (feat) adding twilio middleware

--- server/middleware/twilio.js
+++ server/middleware/twilio.js
@@ -1,17 +1,24 @@
 const config = require('config')['twilio'];
 const client = require('twilio')(config.accountSid, config.authToken);
+const LookupsClient = require('twilio').LookupsClient;
+const lookupsClient = new LookupsClient(config.accountSid, config.authToken);
 
-
-module.exports = (to, message) => {
+module.exports.sendSms = (to, message) => {
   return client.api.messages
     .create({
       body: message,
       to: to,
       from: config.number,
     }).then((data) => {
-      console.log('Administor Notified');
+      console.log('Message Sent');
     }).catch((err) => {
-      console.error('Could not notify administrator');
+      console.error('Error has occurred');
       console.error(err);
     });
+};
+
+module.exports.phoneLookUp = (phoneNumber) => {
+  lookupsClient.phoneNumbers(phoneNumber).get((error, number) => {
+    return number.phone_number;
+  });
 };
