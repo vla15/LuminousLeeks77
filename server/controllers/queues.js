8aa002be6146fadfefe054603faa400df6c03c4d
Vincent La <vincentla@Vincents-MacBook-Pro.local>
2017-08-01 11:59:22 -0700

    (feat) incorporating a back button to go back to queue list

--- server/controllers/queues.js
+++ server/controllers/queues.js
@@ -51,8 +51,7 @@ module.exports.getAll = (req, res) => {
     });
 };
 
-//grabs all parties info, for example: http://localhost:3000/api/queueinfo/host/1
-//add parties: http://localhost:3000/api/partyinfo/add/1/1/4
+
 module.exports.getPartyInfoOfQueue = (req, res, next) => {
   SocketIO.sendSocketDataForParties(req.params.queueid);
   res.send('success');
