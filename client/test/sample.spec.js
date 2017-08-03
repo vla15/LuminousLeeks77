595033139c391c1de1bf746420c0ce719afd7bbf
Sheyda Rezaei <sheydarezaee@gmail.com>
2017-08-03 10:00:14 -0700

    tests MyComponent with shallow, mount anf render methods

--- client/test/sample.spec.js
+++ client/test/sample.spec.js
@@ -5,27 +5,27 @@ import expect from 'expect';
 
 import { shallow, mount, render } from 'enzyme';
 
-const SampleComponent = (props = {}) => (
-  <div className="sample">
-    Sample
+const MyComponent = (props = {}) => (
+  <div className="unique">
+    My Component
   </div>
 );
 
 describe('A sample react component', function() {
 
-  it('should render without throwing an error', function() {
-    expect(shallow(<SampleComponent />).contains(<div className="sample">Sample</div>)).toBe(true);
+  it('should render My Component', function() {
+    expect(shallow(<MyComponent />).contains(<div className="unique">My Component</div>)).toBe(true);
   });
 
-  it('should be selectable by class "sample"', function() {
-    expect(shallow(<SampleComponent />).is('.sample')).toBe(true);
+  it('should be selectable by class "unique"', function() {
+    expect(shallow(<MyComponent />).is('.unique')).toBe(true);
   });
 
-  // it('should mount in a full DOM', function() {
-  //   expect(mount(<SampleComponent />).find('.sample').length).toBe(1);
+  // it('should mount MyComponent in a full DOM', function() {
+  //   expect(mount(<MyComponent />).find('.unique').length).toBe(1);
   // });
 
   it('should render to static HTML', function() {
-    expect(render(<SampleComponent />).text()).toEqual('Sample');
+    expect(render(<MyComponent />).text()).toEqual('My Component');
   });
 });
